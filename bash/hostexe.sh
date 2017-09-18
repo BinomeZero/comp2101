dc9c443ea96a4dd5dd102216771fd058febd7ecd
Brendan <brendan.naismith@gmail.com>
2017-09-18 06:21:35 -0700

    fixed

--- bash/hostexe.sh
+++ bash/hostexe.sh
@@ -1,6 +1,7 @@
-!#/bin/bash
-export $HOSTNAME=("Sleep")
-titlename=("The Tired One")
+#!/bin/bash
+export HOSTNAME=$"Sleep"
+titlename=$"The Tired One"
 day=$(date +%A)
 
-echo "Welcome to planet $HOSTNAME, o'$titlename, todays date is $day";
+echo "Welcome to planet $HOSTNAME, o'$titlename"
+echo "todays date is $day"
